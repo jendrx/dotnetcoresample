09613e0cd0149d5ac9b3dc1d5bac8ad96b18c875
rmendonca <romendonca@ua.pt>
2020-03-28 19:28:13 +0000

    Added ftestproject, and modified Dockerfile.

---
+++ UtilLib.FTest/DummyFTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UtilLib.FTest
+{
+    public class DummyFTest
+    {
+        [Fact]
+        public void AnotherDummyTest()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
